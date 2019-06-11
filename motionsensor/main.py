e52fa424fc088f868e19a9cfd5022a699d9a44f5
Marcin <marcin@szczeliniak.pl>
2019-06-11 21:42:27 +0200

    ref

--- motionsensor/main.py
+++ motionsensor/main.py
@@ -3,6 +3,7 @@ from motionsensor.config import load_config
 from motionsensor.android_connector import AndroidConnector
 from motionsensor.face_rec import FaceRecognition
 import urllib
+import time
 
 import sys
 import os
@@ -32,16 +33,18 @@ print("============================================")
 try:
     while(True):
         if ms.detect() == True:
-            print("Motion detected! Trying to recognize face.")
+            print("Motion detected!")
+            print("Downloading actual photo.")
             connector.download_photo_to_tmp_folder()
             comparision_result = face_recognition.compare(connector.path_to_tmp_file)
             if comparision_result is not None:
+                print("========================================")
                 print("User recognized! It is %s! Door is being opened!" % comparision_result)
+                time.sleep(3)
+                print("========================================"
             else:
-                print("A stranger has arrived. Door is being kept locked.")
-        else:
-            print("Motion not detected.")
+                print("Face not recognized.")
 except KeyboardInterrupt as e:
     print("Program stopped.")
 except urllib.error.URLError as e:
-    print("Connection with server could not get established or has been lost. Program ends.")
+    print("Connection with server could not get established or has been lost. Program is being ended.")
